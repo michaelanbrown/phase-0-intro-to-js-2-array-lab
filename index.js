059fd905ca37870f83368a894ccabb81466c63bb
Michaela Brown <m_brownbrown@yahoo.com>
2022-09-27 20:24:09 -0700

    commit tree 201c0acc0c6409373056f07389c3e64b19903f77

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCatSlice = cats.slice(0, -1);
+    return newCatSlice;
+}
+
+function removeFirstCat() {
+    const newCatSlice = cats.slice(1);
+    return newCatSlice;
+}
